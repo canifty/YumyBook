5d39970e89cbc0e30a4ec5112507b5837f615dcb
canifty <can-dindar@outlook.com>
2024-08-26 07:43:51 -0400

    Initial Commit

---
+++ YumyBook/YumyBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumyBookApp.swift
+//  YumyBook
+//
+//  Created by Can Dindar on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct YumyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
